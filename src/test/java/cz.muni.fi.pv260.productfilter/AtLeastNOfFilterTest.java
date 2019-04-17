48d42915bd498bcdcf1cece1618362e2a590127e
xracek5 <445411@mail.muni.cz>
2019-04-17 21:53:49 +0200

    Added simple test case which always pass

---
+++ src/test/java/cz.muni.fi.pv260.productfilter/AtLeastNOfFilterTest.java
@@ -0,0 +1,14 @@
+package cz.muni.fi.pv260.productfilter;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AtLeastNOfFilterTest {
+
+    @Test
+    public void alwaysPass() {
+        assertTrue(true);
+    }
+}
